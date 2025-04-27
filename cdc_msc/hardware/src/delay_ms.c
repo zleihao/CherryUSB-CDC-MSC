9e687906e8e2163e62d17ffd265b44c816541977
hao <im_leihao@163.com>
2025-04-27 16:58:26 +0800

    修复ms延时

--- cdc_msc/hardware/src/delay_ms.c
+++ cdc_msc/hardware/src/delay_ms.c
@@ -1,4 +1,5 @@
 #include "delay_ms.h"
+#include <stdint.h>
 
 //定时器
 void timer_init(void)
@@ -10,18 +11,25 @@ void timer_init(void)
     TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
     TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
     TIM_TimeBaseStructure.TIM_Period = 0xFFFF;
-    TIM_TimeBaseStructure.TIM_Prescaler = (84000000 / 1000) - 1;
+    TIM_TimeBaseStructure.TIM_Prescaler = (84000000 / 10000) - 1;
 
     TIM_TimeBaseInit(TIM6, &TIM_TimeBaseStructure);
 }
 
 void delay_ms(uint16_t tms)
 {
+    uint32_t counter = tms * 10;
+
+    if (counter > 0xFFFF) {
+        counter = 0xFFFF;
+    }
+
     TIM6->CNT = 0;
+    TIM_ClearFlag(TIM6, TIM_FLAG_Update); // 清除溢出标志
 
     TIM_Cmd(TIM6, ENABLE);
 
-    while (TIM6->CNT < tms) {
+    while (TIM6->CNT < counter) {
         ;
     }
 
